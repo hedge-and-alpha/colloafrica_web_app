c40cd1b1e657ac02fa55c76c4710dbae59b8fda7
Kevin Vantus <kevinvantus@gmail.com>
2024-04-17 23:23:20 +0100

    chore: add security template

--- src/app/modules/dashboard/pages/profile/security/security.component.ts
+++ src/app/modules/dashboard/pages/profile/security/security.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ColsField3Component } from '../../../../../components/cols-field-3/cols-field-3.component';
+import { FormFieldComponent } from '../../../../../components/form-field/form-field.component';
+import { CardComponent } from '../../../components/card/card.component';
+import { ProfileCardComponent } from '../../../components/profile-card/profile-card.component';
 
 @Component({
   selector: 'ca-security',
   standalone: true,
   templateUrl: './security.component.html',
   styleUrl: './security.component.css',
+  imports: [
+    CardComponent,
+    ColsField3Component,
+    FormFieldComponent,
+    ProfileCardComponent,
+  ],
 })
 export class SecurityComponent {}
