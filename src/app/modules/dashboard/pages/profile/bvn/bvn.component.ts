e2d72dfb999d7734dcf2ebe771baaccf3eb9ddc2
Kevin Vantus <kevinvantus@gmail.com>
2024-04-17 23:33:44 +0100

    chore: add bvn info template

--- src/app/modules/dashboard/pages/profile/bvn/bvn.component.ts
+++ src/app/modules/dashboard/pages/profile/bvn/bvn.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../../components/card/card.component';
+import { ColsField3Component } from '../../../../../components/cols-field-3/cols-field-3.component';
+import { FormFieldComponent } from '../../../../../components/form-field/form-field.component';
+import { ProfileCardComponent } from '../../../components/profile-card/profile-card.component';
 
 @Component({
   selector: 'ca-bvn',
   standalone: true,
   templateUrl: './bvn.component.html',
   styleUrl: './bvn.component.css',
+  imports: [
+    CardComponent,
+    ColsField3Component,
+    FormFieldComponent,
+    ProfileCardComponent,
+  ],
 })
 export class BvnComponent {}
