606b7caf0dd2ea1ab1db1571f41aea5519efa311
Kevin Vantus <kevinvantus@gmail.com>
2024-04-21 02:46:01 +0100

    feature: click outside sidebar to close

--- src/app/modules/dashboard/components/header/header.component.ts
+++ src/app/modules/dashboard/components/header/header.component.ts
@@ -1,34 +1,8 @@
-import { Component, EventEmitter, Output, inject } from '@angular/core';
-import {
-  Router,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-} from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ca-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-  isNavOpen = false;
-  router = inject(Router);
-
-  @Output() toggleChange = new EventEmitter<boolean>();
-
-  ngOnInit() {
-    this.router.events.subscribe({
-      next: (e) => {
-        if (
-          e instanceof NavigationEnd ||
-          e instanceof NavigationCancel ||
-          e instanceof NavigationError
-        ) {
-          this.isNavOpen = false;
-          this.toggleChange.emit(this.isNavOpen);
-        }
-      },
-    });
-  }
-}
+export class HeaderComponent {}
--- src/app/modules/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/modules/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  Router,
+} from '@angular/router';
 
 @Component({
   selector: 'ca-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css',
+  host: {
+    '(document:click)': 'onClickOutsideMainNav($event)',
+  },
 })
 export class DashboardLayoutComponent {
-  showNav = false;
+  showNav = signal(false);
+
+  nav = viewChild<ElementRef>('mainNav');
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe({
+      next: (e) => {
+        if (
+          e instanceof NavigationEnd ||
+          e instanceof NavigationCancel ||
+          e instanceof NavigationError
+        ) {
+          this.showNav.set(false);
+        }
+      },
+    });
+  }
+
+  toggleNav(e: Event) {
+    if (!this.showNav()) {
+      e.stopPropagation();
+    }
+    this.showNav.set(!this.showNav());
+  }
+
+  onClickOutsideMainNav(event: Event) {
+    const navEl = this.nav()?.nativeElement as HTMLElement;
+    const clickedOutside = navEl.contains(event.target as Node);
+
+    if (!clickedOutside) {
+      this.showNav.set(false);
+    }
+  }
 }
