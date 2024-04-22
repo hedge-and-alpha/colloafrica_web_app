ee72cbde8bb2c927b46882a2ee99db55753b3adb
Kevin Vantus <kevinvantus@gmail.com>
2024-04-22 16:26:18 +0100

    test: analytics component

--- src/app/modules/dashboard/components/analytics/analytics.component.spec.ts
+++ src/app/modules/dashboard/components/analytics/analytics.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AnalyticsComponent } from './analytics.component';
+import { AnalyticsCardComponent } from '../analytics-card/analytics-card.component';
 
 describe('AnalyticsComponent', () => {
   let component: AnalyticsComponent;
@@ -8,10 +9,9 @@ describe('AnalyticsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AnalyticsComponent]
-    })
-    .compileComponents();
-    
+      declarations: [],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AnalyticsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,19 @@ describe('AnalyticsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render four analytics cards with icons', () => {
+    const analyticsComponent: HTMLElement = fixture.debugElement.nativeElement;
+    const cards = analyticsComponent.querySelectorAll('ca-analytics-card');
+    const icons: HTMLElement[] = [];
+
+    cards.forEach((card) => {
+      const i = card.querySelector('i');
+      i && icons.push(i);
+    });
+
+    expect(cards.length).toBe(4);
+    expect(icons.length).toEqual(4);
   });
 });
