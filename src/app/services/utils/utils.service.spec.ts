e833cd2a19de263aed7cd05422fd23913d4a5e5e
Kevin Vantus <kevinvantus@gmail.com>
2024-04-25 02:46:40 +0100

    test: utils service

--- src/app/services/utils/utils.service.spec.ts
+++ src/app/services/utils/utils.service.spec.ts
@@ -13,4 +13,29 @@ describe('UtilsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create slug from given text', () => {
+    const text = 'I love utils service';
+    const slug = 'i-love-utils-service';
+
+    expect(service.createSlugFromText(text)).toEqual(slug);
+  });
+
+  it('should capitalize first letter of sentence or first word', () => {
+    const text = 'love utils service';
+    const capitalized = 'Love utils service';
+
+    expect(service.capitalizeFirstLetterOfWord(text)).toEqual(capitalized);
+    expect(service.capitalizeFirstLetterOfWord(text)).not.toEqual(
+      'lOve utiLs serviCe'
+    );
+    expect(service.capitalizeFirstLetterOfWord('utils')).toEqual('Utils');
+  });
+
+  it('should create text from given slug', () => {
+    const text = 'i love utils service';
+    const slug = 'i-love-utils-service';
+
+    expect(service.createTextFromSlug(slug)).toEqual(text);
+  });
 });
