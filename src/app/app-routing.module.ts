0da52a9428cdea613209a932e57ad35f36a2eee5
Kevin Vantus <kevinvantus@gmail.com>
2024-05-16 04:12:19 +0100

    feature: add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
       import('./modules/dashboard/dashboard.module').then(
         (m) => m.DashboardModule
       ),
+    canActivate: [authGuard],
   },
   {
     path: 'auth',
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const token = localStorage.getItem('AUTH_TOKEN');
+  const router = inject(Router);
+  const auth = inject(AuthService);
+
+  if (!token) {
+    auth.url = state.url;
+    router.navigate(['/auth']);
+    return false;
+  }
+  return true;
+};
