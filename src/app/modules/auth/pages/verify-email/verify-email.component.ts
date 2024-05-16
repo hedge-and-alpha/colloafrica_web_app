f0bc04a0066ac6d177ae6f9c2c11f3da41352077
Kevin Vantus <kevinvantus@gmail.com>
2024-05-16 03:29:49 +0100

    api: verify email

--- src/app/modules/auth/pages/verify-email/verify-email.component.ts
+++ src/app/modules/auth/pages/verify-email/verify-email.component.ts
@@ -5,6 +5,12 @@ import { AuthFormLayoutComponent } from '../../layouts/auth-form-layout/auth-for
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { emptyFieldValidator } from '../../../../validators/emptyField.validator';
 import { FormErrorComponent } from '../../../../components/form-error/form-error.component';
+import { AuthApiService } from '../../../../services/auth/auth-api.service';
+import { Router } from '@angular/router';
+import { ButtonLoadingDirective } from '../../../../directives/button-loading/button-loading.directive';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertService } from '../../../../components/alert/alert.service';
+import { AuthService } from '../../../../services/auth/auth.service';
 
 @Component({
   selector: 'ca-verify-email',
@@ -14,6 +20,7 @@ import { FormErrorComponent } from '../../../../components/form-error/form-error
   imports: [
     ReactiveFormsModule,
     AuthFormLayoutComponent,
+    ButtonLoadingDirective,
     ButtonPrimaryDirective,
     FormErrorComponent,
     OtpComponent,
@@ -21,26 +28,69 @@ import { FormErrorComponent } from '../../../../components/form-error/form-error
 })
 export class VerifyEmailComponent {
   isSubmitted = false;
+  loading = false;
+  resending = false;
 
   form = this.fb.group(
     {
       otp: [
-        '1234567',
+        null,
         [Validators.required, Validators.maxLength(6), emptyFieldValidator],
       ],
     },
     { updateOn: 'submit' }
   );
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private api: AuthApiService,
+    private alertService: AlertService,
+    private auth: AuthService
+  ) {}
+
+  resendVerificationCode() {
+    this.resending = true;
+    this.api.resendVerificationCode(this.auth.email!).subscribe({
+      next: ({ message, status }) => {
+        this.resending = false;
+        this.alertService.open('success', {
+          summary: status,
+          details: message,
+        });
+      },
+      error: (error: HttpErrorResponse) => {
+        this.resending = false;
+        this.alertService.open('danger', {
+          summary: error.error.status + ' ' + error.status,
+          details: error.error.message,
+        });
+      },
+    });
+  }
 
   handleSubmit() {
     this.isSubmitted = true;
 
-    console.log(this.form.controls.otp);
-
     if (this.form.invalid) return;
+    this.loading = true;
 
-    console.log('value:', this.form.value);
+    this.api.EmailVerification(this.form.value.otp!).subscribe({
+      next: ({ message, status }) => {
+        this.router.navigate(['/auth']);
+        this.loading = false;
+        this.alertService.open('success', {
+          summary: status,
+          details: message,
+        });
+      },
+      error: (error: HttpErrorResponse) => {
+        this.loading = false;
+        this.alertService.open('danger', {
+          summary: error.error.status + ' ' + error.status,
+          details: error.error.message,
+        });
+      },
+    });
   }
 }
