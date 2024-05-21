d01f4f4c33e20714f5ec69b9a091caa50f6ea13e
Kevin Vantus <kevinvantus@gmail.com>
2024-05-21 23:09:36 +0100

    chore: account wallet placeholder

--- src/app/modules/dashboard/components/wallet-account-details/wallet-account-details.component.ts
+++ src/app/modules/dashboard/components/wallet-account-details/wallet-account-details.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { UserStoreService } from '../../../../stores+/user.store';
 
 @Component({
   selector: 'ca-wallet-account-details',
   templateUrl: './wallet-account-details.component.html',
-  styleUrl: './wallet-account-details.component.css'
+  styleUrl: './wallet-account-details.component.css',
 })
 export class WalletAccountDetailsComponent {
+  user = computed(() => this.userStore.user!);
 
+  constructor(private userStore: UserStoreService) {}
 }
