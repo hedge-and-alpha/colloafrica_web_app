95901933613fd8aaa4caffa8a432cf42ba9cd943
Kevin Vantus <kevinvantus@gmail.com>
2024-06-26 00:11:48 +0100

    chore: mgr stats

--- src/app/modules/dashboard/pages/mgr-details/pages/mgr-contribution-statistics/mgr-contribution-statistics.component.ts
+++ src/app/modules/dashboard/pages/mgr-details/pages/mgr-contribution-statistics/mgr-contribution-statistics.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TableHeading } from '../../../../../../interfaces/table-heading';
+import { MGRContributionStats } from '../../../../../../interfaces/mgr.interface';
 
 @Component({
   selector: 'ca-mgr-contribution-statistics',
   templateUrl: './mgr-contribution-statistics.component.html',
-  styleUrl: './mgr-contribution-statistics.component.css'
+  styleUrl: './mgr-contribution-statistics.component.css',
 })
 export class MgrContributionStatisticsComponent {
+  tableHeading = TABLE_HEADING;
 
+  @Input({ required: true }) contributionStats: MGRContributionStats[] = [];
 }
+
+const TABLE_HEADING: TableHeading[] = [
+  { label: 'Name' },
+  { label: 'Amount' },
+  { label: 'Role' },
+  { label: 'Position' },
+  { label: 'Status' },
+];
