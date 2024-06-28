2e81a201f5969814f66c814b57e3fec4601f1560
Kevin Vantus <kevinvantus@gmail.com>
2024-06-28 09:18:29 +0100

    refactor: remove showcase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ShowcaseComponent } from './showcase/showcase.component';
 import { authGuard } from './guards/auth/auth.guard';
 import { ErrorPageComponent } from './modules/dashboard/components/error-page/error-page.component';
 
@@ -19,7 +18,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
-  { path: 'showcase', component: ShowcaseComponent },
   { path: 'error', component: ErrorPageComponent },
   {
     path: '**',
