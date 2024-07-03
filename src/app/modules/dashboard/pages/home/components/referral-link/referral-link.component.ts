4447371731d8fef7ae934053ae2cdddd5c6d8c9f
Kevin Vantus <kevinvantus@gmail.com>
2024-07-03 01:15:24 +0100

    feat: add referral link to dashboard landing page

--- src/app/modules/dashboard/pages/home/components/referral-link/referral-link.component.ts
+++ src/app/modules/dashboard/pages/home/components/referral-link/referral-link.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { UserStoreService } from '../../../../../../stores+/user.store';
 
 @Component({
   selector: 'ca-referral-link',
   templateUrl: './referral-link.component.html',
 })
-export class ReferralLinkComponent {}
+export class ReferralLinkComponent {
+  referralLink!: string;
+
+  user = computed(() => this.userStore.user);
+
+  constructor(private userStore: UserStoreService) {
+    this.referralLink = `${location.origin}/auth/sign-up?referral_code=${
+      this.user()!.referral_code
+    }`;
+  }
+}
