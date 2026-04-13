d2287ba348e8b9fc5df0a826884988312d850f2b
misspelz <adetoyeofficial@gmail.com>
2026-04-13 16:35:33 +0100

    chore: add eye toggle to register password fields

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@
 
 export const environment = {
   production: false,
-  API_BASE_URL: window.location.hostname == 'localhost' ? 'http://127.0.0.1:8000/api/v1' : 'https://staging-server.colloafrica.com/api/v1',
+  API_BASE_URL: "https://live-collo-backend.colloafrica.com/api/v1",
+  // API_BASE_URL: window.location.hostname == 'localhost' ? 'http://127.0.0.1:8000/api/v1' : 'https://staging-server.colloafrica.com/api/v1',
 };
